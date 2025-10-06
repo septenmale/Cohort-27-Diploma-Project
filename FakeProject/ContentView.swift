a0c76d99669107e2c5556daca13c70791fe5be05
septenmale <zavhorodnii.viktor@icloud.com>
2025-10-06 11:51:32 +0200

    first commit

---
+++ FakeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeProject
+//
+//  Created by Viktor Zavhorodnii on 06/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeProject/FakeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeProjectApp.swift
+//  FakeProject
+//
+//  Created by Viktor Zavhorodnii on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
